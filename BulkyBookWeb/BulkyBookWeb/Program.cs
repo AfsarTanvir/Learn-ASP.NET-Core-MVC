8fe3e679f3549b1459cae0e13d2cbb1f39924265
AfsarTanvir <afsartanvir805@gmail.com>
2025-01-17 02:35:09 +0600

    Database Connected Successfully

--- BulkyBookWeb/BulkyBookWeb/Program.cs
+++ BulkyBookWeb/BulkyBookWeb/Program.cs
@@ -1,3 +1,7 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 namespace BulkyBookWeb
 {
     public class Program
@@ -9,6 +13,9 @@ namespace BulkyBookWeb
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.

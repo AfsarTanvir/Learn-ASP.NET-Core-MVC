b3c2253965aea6694dd444fdb55e000291e1dbaf
AfsarTanvir <afsartanvir805@gmail.com>
2025-01-17 06:01:51 +0600

    After Adding Theme

--- BulkyBookWeb/BulkyBookWeb/Program.cs
+++ BulkyBookWeb/BulkyBookWeb/Program.cs
@@ -12,10 +12,10 @@ namespace BulkyBookWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-            builder.Services.AddRazorPages();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
